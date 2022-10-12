343ab2426f98ea3b65f23af09f9993615502edb5
Christian Aistleitner <christian@quelltextlich.at>
2022-10-12 17:05:10 +0200

    4email: Add processing of message answers

--- halloelterncli/halloeltern4email.py
+++ halloelterncli/halloeltern4email.py
@@ -37,27 +37,41 @@ class HalloElternApp4Email(object):
         self._api = api.Api(self._config)
         seen_ids_file = config.get('base', 'seen-ids-file')
         self._seen_ids_store = idstore.IdStore(seen_ids_file)
+        self._converter = messagetoemailconverter.MessageToEmailConverter(
+            self._config, self._api.get_authenticated_user())
+
+    def deliver(self, message, mda, extra_data, process_all, parent=None):
+        confirmed_status = 'confirmed' if 'confirmed_by' in message \
+            else 'unconfirmed'
+        id = f"{message['itemid']}-{confirmed_status}"
+
+        if process_all or not self._seen_ids_store.has_been_seen(id):
+            email = self._converter.convert(message, extra_data, parent)
+            mda.deliver(email)
 
     def run(self, mda, process_all=False):
-        converter = messagetoemailconverter.MessageToEmailConverter(
-            self._config, self._api.get_authenticated_user())
         for pinboard in self._api.list_pinboards():
             pinboard_id = pinboard['itemid']
             child_code = pinboard['code']
 
-            for message in self._api.list_messages(pinboard_id, child_code):
-                confirmed_status = 'confirmed' if 'confirmed_by' in message \
-                    else 'unconfirmed'
-                id = f"{message['itemid']}-{confirmed_status}"
-                if process_all or not self._seen_ids_store.has_been_seen(id):
-                    email = converter.convert(message, {
-                            'pinboard_id': pinboard_id,
-                            'school_name': pinboard['school'],
-                            'class_name': pinboard['subtitle'],
-                            'child_name': pinboard['title'],
-                            'child_code': child_code,
-                            })
-                    mda.deliver(email)
+            extra_data = {
+                'pinboard_id': pinboard_id,
+                'school_name': pinboard['school'],
+                'class_name': pinboard['subtitle'],
+                'child_name': pinboard['title'],
+                'child_code': child_code,
+
+                }
+            for abstract in self._api.list_messages(pinboard_id, child_code):
+                id = abstract['itemid']
+                message = self._api.get_message(id, child_code)
+
+                self.deliver(message, mda, extra_data, process_all)
+                for answer in message['answers']:
+                    if answer['message']:
+                        self.deliver(answer, mda, extra_data, process_all,
+                                     parent=message)
+
             self._seen_ids_store.persist()
 
 
